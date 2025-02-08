5b74dd2a290bb5b947dce6b791f2d70534aec626
Arthur <arthurvpires@live.com>
2025-02-08 16:16:31 -0300

    feat: setup api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/task/create', [TaskController::class, 'create']);
+Route::get('/task/list', [TaskController::class, 'list']);
+Route::post('/task/list/{id}', [TaskController::class, 'listDetails']);
+Route::put('/task/update/{id}', [TaskController::class, 'update']);
+Route::delete('/task/destroy/{id}', [TaskController::class, 'destroy']);
